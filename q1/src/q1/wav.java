989a265b7610ab592bf4ae782d2d0bea67365692
Matin <Matin.keivanloo@yahoo.ca>
2024-12-22 22:08:39 -0800

    Update wav.java

--- q1/src/q1/wav.java
+++ q1/src/q1/wav.java
@@ -2,37 +2,38 @@ package q1;
 
 import java.io.File;
 import java.io.IOException;
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 
 public class wav {
-	public static File compress(File f, String pfile, int count) throws InterruptedException, UnsupportedAudioFileException, IOException {
-	    File file = f;
-	    File output = new File(pfile + "/compressed" + count + ".wav");
+	public File compress(File inputFile, String outputDirectory, int count) throws IOException, UnsupportedAudioFileException {
+		File outputFile = new File(outputDirectory + "/compressed" + count + ".wav");
 
-	    AudioInputStream ais;
-	    AudioInputStream eightKhzInputStream = null;
-	    ais = AudioSystem.getAudioInputStream(file);
-	    AudioFormat sourceFormat = ais.getFormat();
-	    if (ais.getFormat().getSampleRate() != 22050f) {
-	        AudioFileFormat sourceFileFormat = AudioSystem.getAudioFileFormat(file);
-	        AudioFileFormat.Type targetFileType = sourceFileFormat.getType();
+		AudioInputStream inputAudioStream = AudioSystem.getAudioInputStream(inputFile);
+		AudioFormat sourceFormat = inputAudioStream.getFormat();
 
-	        AudioFormat targetFormat = new AudioFormat(
-	                sourceFormat.getEncoding(),20000f,sourceFormat.getSampleSizeInBits(),sourceFormat.getChannels(),
-	                sourceFormat.getFrameSize(),
-	                20000f,
-	                sourceFormat.isBigEndian());
-	        if (!AudioSystem.isFileTypeSupported(targetFileType) || ! AudioSystem.isConversionSupported(targetFormat, sourceFormat)) {
-	              throw new IllegalStateException("Conversion not supported!");
-	        }
-	        eightKhzInputStream = AudioSystem.getAudioInputStream(targetFormat, ais);
-	        int nWrittenBytes = 0;
-	        nWrittenBytes = AudioSystem.write(eightKhzInputStream, targetFileType, output);
-	    }
-	    return output;
+		// Target Format for Compression
+		AudioFormat targetFormat = new AudioFormat(
+				sourceFormat.getEncoding(),
+				20000f, // Reduced Sample Rate
+				sourceFormat.getSampleSizeInBits(),
+				sourceFormat.getChannels(),
+				sourceFormat.getFrameSize(),
+				20000f,
+				sourceFormat.isBigEndian()
+		);
+
+		if (!AudioSystem.isConversionSupported(targetFormat, sourceFormat)) {
+			throw new UnsupportedAudioFileException("Conversion to the target format is not supported.");
+		}
+
+		AudioInputStream compressedStream = AudioSystem.getAudioInputStream(targetFormat, inputAudioStream);
+
+		// Write the compressed audio stream to the output file
+		AudioSystem.write(compressedStream, AudioFileFormat.Type.WAVE, outputFile);
+
+		inputAudioStream.close();
+		compressedStream.close();
+
+		return outputFile;
 	}
 }
